a7e3d03d3f72f821b8cf5ac5c23f869dae603956
AlexisKH <n01653801@humber.ca>
2026-04-13 21:30:05 -0400

    added posts/articles and css changes

--- weather-app/components/Navbar.js
+++ weather-app/components/Navbar.js
@@ -1,18 +1,29 @@
 import Link from "next/link";
+<<<<<<< Updated upstream
 import Image from "next/image";
 import ThemeSwitcher from "@/components/ThemeSwitcher"
+=======
+>>>>>>> Stashed changes
 
 export default function Navbar(){
     return(
         <nav>
             <div className="logo">
-                <Image src="/logo.png" alt="logo" width={80} height={42} />
+                <img src="/logo.png" alt="logo" width={80} height={42} />
             </div>
             <div>
                 <Link href="/">HOME</Link>
                 <Link href="/posts">POSTS</Link>
                 <Link href="/contact">CONTACT</Link>
+<<<<<<< Updated upstream
                 <ThemeSwitcher></ThemeSwitcher>
+=======
+                
+                {/*<select name="theme">
+                    <option value="light">Light</option>
+                    <option value="dark">Dark</option>
+                </select>*/}
+>>>>>>> Stashed changes
             </div>
         </nav>
     )
--- weather-app/pages/posts.tsx
+++ weather-app/pages/posts.tsx
@@ -2,10 +2,50 @@ import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 
 export default function Posts(){
+    const posts = [
+        {
+            id: 1,
+            title: "The Effects of Climate Change",
+            description: "The effects of human-caused global warming are happening now, are irreversible for people alive today, and will worsen as long as humans add greenhouse gases to the atmosphere. ",
+            url: "https://science.nasa.gov/climate-change/effects/",
+            image: "https://assets.science.nasa.gov/dynamicimage/assets/science/esd/climate/internal_resources/2710/Effects_page_triptych.jpeg?w=2963&h=1459&fit=clip&crop=faces%2Cfocalpoint"
+        },
+        {
+            id: 2,
+            title: "How Weather Impacts People's Mental Health",
+            description: "Weather is an integral part of our lives, shaping our daily routines, activities, and even our moods. While it's common to hear people discuss how they feel on a sunny day versus a rainy one, the relationship between weather and mental health is a topic that deserves more attention",
+            url: "https://www.counselling-directory.org.uk/articles/how-weather-impacts-peoples-mental-health",
+            image: "https://cdn.memiah.co.uk/uploads/counselling-directory.org.uk/image_gallery/weather-1693905944-hero.jpg"
+        },
+        {
+            id: 3,
+            title: "How to Stay Safe During Heat Waves",
+            description: "Even in places where heat is recognized as a dangerous health threat, people can be caught off guard as the thermometer creeps higher, on average, each year.",
+            url: "https://theconversation.com/how-to-stay-safe-during-heat-waves-and-heat-stroke-warning-signs-to-watch-for-257708",
+            image: "https://images.theconversation.com/files/673482/original/file-20250610-56-5xxi9q.png?ixlib=rb-4.1.0&q=45&auto=format&w=1000&fit=clip"
+        }
+    ]
     return(
         <div className="layout">
             <Navbar />
-
+            <main>
+                <h1>Articles</h1>
+                <div className="posts-container">
+                    {posts.map(post => (
+                    <a
+                        key={post.id}
+                        href={post.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="card"
+                    >
+                    <img src={post.image} alt={post.title}/>
+                    <h2>{post.title}</h2>
+                    <p>{post.description}</p>
+                    </a>
+                    ))}
+                </div>
+            </main>
             <Footer />
         </div>
     )
